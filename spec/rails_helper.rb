404d6ea88530f9f76f7beda403a3f6bd80617af3
AlexDikun <loban.sasha.97@mail.ru>
2023-12-14 23:50:23 +0300

    SYNTAX ERROR FIX

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,7 +61,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  onfig.include FactoryBot::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
 
   Shoulda::Matchers.configure do |config|
     config.integrate do |with|
