8983d7b85ac1eeee666c2a360f4fe9c6f5767951
AlexDikun <loban.sasha.97@mail.ru>
2023-12-15 00:03:07 +0300

    tests for model validation Product added

--- spec/models/product_model_spec.rb
+++ spec/models/product_model_spec.rb
@@ -4,5 +4,7 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+  it { should validate_presence_of(:price) }
 end
