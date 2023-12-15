0f7710b6453c708d51639babade31c3e891aff85
AlexDikun <loban.sasha.97@mail.ru>
2023-12-15 18:47:27 +0300

    wrote create update destroy actions to ProductsController

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -4,13 +4,40 @@
 class Api::V1::ProductsController < ApplicationController
 
   # POST api/v1/products
-  def create; end
+  def create
+    @product = Product.new(product_params)
+    @product.image_derivatives! if @product.valid?
+    if @product.save
+      render jsonapi: @product, status: :created, code: '201'
+    else
+      render jsonapi_errors: @product.errors,
+             status: :unprocessable_entity, code: '422'
+    end
+  end
 
   # PATCH/PUT api/v1/products/:id
-  def update; end
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      @product.image_derivatives! if @product.valid?
+      @product.save
+      render jsonapi: @product, status: :ok, code: '200'
+    else
+      render jsonapi_errors: @product.errors,
+             status: :unprocessable_entity, code: '422'
+    end
+  end
 
   # DELETE api/v1/products/:id
-  def destroy; end
+  def destroy
+    @product = Product.find(params[:id])
+    if @product.destroy
+      render jsonapi: @product, status: :ok, code: '200'
+    else
+      render jsonapi_errors: @product.errors,
+             status: :unprocessable_entity, code: '422'
+    end
+  end
 
   private
 
