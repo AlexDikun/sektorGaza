53a605f046bb8716d7eaaecbfb88d2bb22f44c23
AlexDikun <loban.sasha.97@mail.ru>
2023-12-15 12:01:59 +0300

    wrote tests for Product model with context valid image format

--- spec/models/product_model_spec.rb
+++ spec/models/product_model_spec.rb
@@ -8,4 +8,16 @@ RSpec.describe Product, type: :model do
   it { should validate_presence_of(:body) }
   it { should validate_presence_of(:price) }
   it { should validate_presence_of(:image) }
+
+  context 'validation image format' do
+    it 'allows to set webp file as an image' do
+      subject.attributes = attributes_for(:product)
+      is_expected.to be_valid
+    end
+
+    it 'disallows to set webp file as an image' do
+      subject.attributes = attributes_for(:product, :with_invalid_image)
+      is_expected.to be_invalid
+    end
+  end
 end
