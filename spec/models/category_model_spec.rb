16a23adfea8904adfcfbbdf4ac44861035be79ba
AlexDikun <loban.sasha.97@mail.ru>
2023-12-16 19:59:50 +0300

    added tests for validations Category model

---
+++ spec/models/category_model_spec.rb
@@ -0,0 +1,14 @@
+# spec/models/category_model_spec.rb
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+  subject { build(:category) }
+
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:description) }
+  it { is_expected.to validate_length_of(:name).is_at_most(30) }
+  it { is_expected.to validate_length_of(:description).is_at_most(100) }
+  it { is_expected.to validate_uniqueness_of(:name) }
+end
