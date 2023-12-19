2b6373d51769345b24511094c4593fcd242fcee1
AlexDikun <loban.sasha.97@mail.ru>
2023-12-19 15:44:19 +0300

    wrote create-update-destroy actions to CategoriesController

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -3,11 +3,41 @@
 
 class Api::V1::CategoriesController < ApplicationController
   # POST api/v1/categories
-  def create; end
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      render jsonapi: @category, status: :created, code: '201'
+    else
+      render jsonapi_errors: @category.errors,
+             status: :unprocessable_entity, code: '422'
+    end
+  end
 
   # PATCH/PUT api/v1/categories/:id
-  def update; end
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      render jsonapi: @category, status: :ok, code: '200'
+    else
+      render jsonapi_errors: @category.errors,
+             status: :unprocessable_entity, code: '422'
+    end
+  end
 
   # DELETE api/v1/categories/:id
-  def destroy; end
+  def destroy
+    @category = Category.find(params[:id])
+    if @category.destroy
+      render jsonapi: @category, status: :ok, code: '200'
+    else
+      render jsonapi_errors: @category.errors,
+             status: :unprocessable_entity, code: '422'
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :description)
+  end
 end
