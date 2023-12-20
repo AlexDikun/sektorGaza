a682a3297fcdecc298ebb52d27a28762399f89c7
AlexDikun <loban.sasha.97@mail.ru>
2023-12-20 18:35:33 +0300

    wrote destroy-action to CategorizationsController

--- app/controllers/api/v1/categorizations_controller.rb
+++ app/controllers/api/v1/categorizations_controller.rb
@@ -6,5 +6,13 @@ class Api::V1::CategorizationsController < Api::V1::BaseController
   def create; end
 
   # DELETE api/v1/categorizations/:id
-  def destroy; end
+  def destroy
+    @categorization = Categorization.find(params[:id])
+    if @categorization.destroy
+      render jsonapi: @categorization, status: :ok, code: '200'
+    else
+      render jsonapi_errors: @categorization.errors,
+             status: :unprocessable_entity, code: '422'
+    end
+  end
 end
