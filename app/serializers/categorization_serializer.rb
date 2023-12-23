6ee7c9490307d57ddf2163892eec3755f8398eaf
AlexDikun <loban.sasha.97@mail.ru>
2023-12-23 17:33:25 +0300

    added annotate

--- app/serializers/categorization_serializer.rb
+++ app/serializers/categorization_serializer.rb
@@ -1,5 +1,15 @@
+# == Schema Information
+#
+# Table name: categorizations
+#
+#  id          :bigint           not null, primary key
+#  category_id :integer
+#  product_id  :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class CategorizationSerializer
   include JSONAPI::Serializer
-  belongs_to :category
+  belongs_to :category 
   belongs_to :product
 end
