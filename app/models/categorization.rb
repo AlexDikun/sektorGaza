0d69bee2555dd43459fec8df0984ca9076f0a756
AlexDikun <loban.sasha.97@mail.ru>
2023-12-24 12:43:52 +0300

    update model - added annotate

--- app/models/categorization.rb
+++ app/models/categorization.rb
@@ -1,3 +1,16 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: categorizations
+#
+#  id          :bigint           not null, primary key
+#  category_id :integer
+#  product_id  :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 # app/models/categorization.rb
 # frozen_string_literal: true
 
