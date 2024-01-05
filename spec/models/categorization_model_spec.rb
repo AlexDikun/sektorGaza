fe5657d544bc9ae62da44da79cf489f84b1f1990
AlexDikun <loban.sasha.97@mail.ru>
2024-01-05 22:45:04 +0300

    added validation for the uniqueness of parameters of the category_id: :product_id to categorization_model_spec

--- spec/models/categorization_model_spec.rb
+++ spec/models/categorization_model_spec.rb
@@ -6,6 +6,14 @@ require 'rails_helper'
 RSpec.describe Categorization, type: :model do
   it { is_expected.to belong_to :category }
   it { is_expected.to belong_to :product }
+  it do
+    should validate_uniqueness_of(:category_id)
+      .scoped_to(:product_id).case_insensitive
+  end
+  it do
+    should validate_uniqueness_of(:product_id)
+      .scoped_to(:category_id).case_insensitive
+  end
 
   let(:category) { create :category }
   let(:product) { create :product }
