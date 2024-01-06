85cfe3512bf79006fee9e5640f722e5cb2558270
AlexDikun <loban.sasha.97@mail.ru>
2024-01-06 20:08:29 +0300

    added rspec test-file for User models and requests

---
+++ spec/models/user_model_spec.rb
@@ -0,0 +1,8 @@
+# spec/models/user_model_spec.rb
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  
+end
---
+++ spec/requests/api/v1/users_requests_spec.rb
@@ -0,0 +1,8 @@
+# spec/requests/api/users_requests_spec.rb
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Api::V1::Users', type: :request do
+  
+end
