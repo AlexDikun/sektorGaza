6276f566cfd97247ccad05d1b63bc97a5b605c7d
AlexDikun <loban.sasha.97@mail.ru>
2024-01-07 22:25:34 +0300

    fix errors FUCKING CTRL+C CTRL+V

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::BaseController < ApplicationController
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[fullname bio avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[fullname avatar])
   end
 
   def after_sign_in_path_for(_user)
