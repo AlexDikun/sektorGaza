2a75587be8021b1884d928d589fb7922340a93c0
AlexDikun <loban.sasha.97@mail.ru>
2024-01-07 22:58:54 +0300

    wrote UPDATE DESTROY actions for userscontroller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,10 +6,28 @@ class Api::V1::UsersController < Api::V1::BaseController
   before_action :set_user,           only: %i[update destroy]
 
   # PATCH/PUT api/v1/users/:id
-  def update; end
+  def update
+    if @user.update(user_update_params)
+      if @user.avatar
+        @user.avatar_derivatives!
+        @user.save
+      end
+      render jsonapi: @user, status: :ok, code: '200'
+    else
+      render jsonapi_errors: @user.errors,
+             status: :unprocessable_entity, code: '422'
+    end
+  end
 
   # DELETE api/v1/users/:id
-  def destroy; end
+  def destroy
+    if @user.destroy
+      render jsonapi: @user, status: :ok, code: '200'
+    else
+      render jsonapi_errors: @user.errors,
+             status: :unprocessable_entity, code: '422'
+    end
+  end
 
   private
 
