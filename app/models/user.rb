03115d972c0aa832400a70213349ee080041d269
AlexDikun <loban.sasha.97@mail.ru>
2024-01-07 20:00:15 +0300

    added validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,5 +16,17 @@
 
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable
+
   include ImageUploader::Attachment(:avatar)
+
+  EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  PASSWORD_FORMAT = /\A(?=.*[a-z])(?=.*[\d]).{8,}\z/i.freeze
+
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: EMAIL_FORMAT, message: 'Invalid email!' }
+  validates :fullname, presence: true, length: { maximum: 50 }
+  validates :password, presence: true, length: { minimum: 8 }, on: :create
+  validates :password, format: { with: PASSWORD_FORMAT,
+                                 message: 'Invalid password!' }, on: :create
+  validates :avatar, presence: true
 end
