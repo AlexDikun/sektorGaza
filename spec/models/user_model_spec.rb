d27683229a7e6c1d192693b11fac74d2b86a5457
AlexDikun <loban.sasha.97@mail.ru>
2024-01-07 19:49:02 +0300

    wrote test validations for user model

--- spec/models/user_model_spec.rb
+++ spec/models/user_model_spec.rb
@@ -4,4 +4,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject { build(:user) }
+
+  it { is_expected.to validate_presence_of(:fullname) }
+  it { is_expected.to validate_length_of(:fullname).is_at_most(50) }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
+  it { is_expected.to validate_presence_of(:password) }
+  it { is_expected.to validate_presence_of(:avatar) }
 end
