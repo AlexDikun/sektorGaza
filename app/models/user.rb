d2babb5775318d974966d3ac17045082ad93c39b
AlexDikun <loban.sasha.97@mail.ru>
2024-01-07 19:40:35 +0300

    refaktorng

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,9 +15,6 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable
   include ImageUploader::Attachment(:avatar)
 end
