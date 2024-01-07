a79afc1cecb8e08b58dddc93fd05f4a3485dbb50
AlexDikun <loban.sasha.97@mail.ru>
2024-01-07 19:41:22 +0300

    create users factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,10 @@
 
 FactoryBot.define do
   factory :account do
+    fullname { FFaker::Name.name }
+    email { FFaker::Internet.email }
+    password { 'password1' }
+    password_confirmation { 'password1' }
+    avatar { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/avatar.png')) }
   end
 end
