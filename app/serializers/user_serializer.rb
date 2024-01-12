068d3e5c5dd2411828e05287db0acbc4b959caed
AlexDikun <loban.sasha.97@mail.ru>
2024-01-12 19:40:32 +0300

    added user serializer

---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                 :bigint           not null, primary key
+#  fullname           :string
+#  avatar_data        :text
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  email              :string           default(""), not null
+#  encrypted_password :string           default(""), not null
+#
+class UserSerializer
+  include JSONAPI::Serializer
+  attributes :fullname, :avatar, :email
+end
