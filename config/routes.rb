3da8ec478f0aa2f971afa3c593f0a4ca338998cc
AlexDikun <loban.sasha.97@mail.ru>
2024-01-12 19:36:11 +0300

    added devise's routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,17 @@ Rails.application.routes.draw do
       get 'catalog_filter', to: 'catalog#filter'
     end
   end
+
+  devise_for :users,
+    defaults: { format: :json },
+    path: '',
+    path_names: {
+      sign_in: 'api/v1/login',
+      sign_out: 'api/v1/logout',
+      registration: 'api/v1/signup'
+    },
+    controllers: {
+      sessions: 'api/v1/sessions',
+      registrations: 'api/v1/registrations'
+    }
 end
