801e17b079a61902a4bf64a658d68f38efd4bfb8
AlexDikun <loban.sasha.97@mail.ru>
2024-01-12 20:04:31 +0300

    fix error: change from username to fullname

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@
 
 FactoryBot.define do
   factory :user do
-    username { FFaker::Name.name }
+    fullname { FFaker::Name.name }
     email { FFaker::Internet.email }
     password { 'password1' }
     avatar { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/avatar.png')) }
