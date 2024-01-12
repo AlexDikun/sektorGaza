3b16d08ef93173158a00215b7a09acdb3d4fe95d
AlexDikun <loban.sasha.97@mail.ru>
2024-01-12 20:10:19 +0300

    wrote tests for the user model

--- spec/models/user_model_spec.rb
+++ spec/models/user_model_spec.rb
@@ -4,4 +4,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject { build(:user) }
+
+  it { is_expected.to validate_presence_of(:fullname) }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_presence_of(:avatar) }
+  it { is_expected.to validate_presence_of(:password) }
+  it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
+  it { is_expected.to validate_length_of(:fullname).is_at_most(30) }
+
+  context 'validation avatar format' do
+    it 'allows to set webp file as an avatar' do
+      subject.attributes = attributes_for(:user)
+      is_expected.to be_valid
+    end
+
+    it 'disallows to set webp file as an avatar' do
+      subject.attributes = attributes_for(:user, :with_invalid_avatar)
+      is_expected.to be_invalid
+    end
+  end
 end
