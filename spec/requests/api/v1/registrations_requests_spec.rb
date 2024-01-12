3b20f8ff803dc3a2f5f43539bcef59d7293fe686
AlexDikun <loban.sasha.97@mail.ru>
2024-01-12 21:19:08 +0300

    wrote test cases for checking user registration

---
+++ spec/requests/api/v1/registrations_requests_spec.rb
@@ -0,0 +1,40 @@
+# spec/requests/api/v1/registrations_requests_spec.rb
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Api::V1::Registrations', type: :request do
+  context 'When creating a new user' do
+    let(:user) { build :user } # to save the email parameter and compare it later
+    let(:params) do
+      { user: { email: user.email,
+                password: user.password } }
+    end
+
+    subject { post 'api/v1/signup', params: params }
+
+    it 'user registration' do
+      expect { subject }.to change { User.count }.by(1)
+      expect(response).to have_http_status(200)
+      expect(response.headers['Authorization']).to be_present
+
+      json_response = JSON.parse(response.body)
+      expect(json_response['data']).to have_attribute(:email).with_value(user.email)
+    end
+  end
+
+  context 'When an email already exists' do 
+    let!(:existing_user) { create :user }
+    let(:params) do
+        { user: { email: existing_user.email,
+                  password: existing_user.password } }
+
+    subject { post 'api/v1/signup', params: params }
+
+    it 'user was unable to register' do
+      expect { subject }.not_to(change { User.count })
+      expect(response).to have_http_status(400)
+    end
+    
+  end
+end
