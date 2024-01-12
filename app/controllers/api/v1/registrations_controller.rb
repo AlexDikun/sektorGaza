c41bd974f6b93f492c45d10a7a15e1504db55be2
AlexDikun <loban.sasha.97@mail.ru>
2024-01-12 20:29:17 +0300

    redefined devise sessions and registrations controllers

---
+++ app/controllers/api/v1/registrations_controller.rb
@@ -0,0 +1,5 @@
+# app/controllers/api/v1/registrations_controller.rb
+# frozen_string_literal: true
+
+class Api::V1::RegistrationsController < Devise::RegistrationsController
+end
---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,5 @@
+# app/controllers/api/v1/sessions_controller.rb
+# frozen_string_literal: true
+
+class Api::V1::SessionsController < Devise::SessionsController
+end
