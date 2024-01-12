c768469ed49d6d590f5781331ee07b84e73fc1bf
AlexDikun <loban.sasha.97@mail.ru>
2024-01-12 19:54:27 +0300

    create User factory

---
+++ spec/factories/users.rb
@@ -0,0 +1,15 @@
+# spec/factories/users.rb
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :product do
+    username { FFaker::Name.name }
+    email { FFaker::Internet.email }
+    password { 'password1' }
+    avatar { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/avatar.png')) }
+
+    trait(:with_invalid_avatar) do
+      image { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/avatar.tiff')) }
+    end
+  end
+end
