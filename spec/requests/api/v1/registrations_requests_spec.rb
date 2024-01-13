c87f5b12e199e0a0e933c80c9eae6338d3aa7228
AlexDikun <loban.sasha.97@mail.ru>
2024-01-13 17:55:37 +0300

    tests have been fixed: the fullname attribute is now sent to the registration post-request

--- spec/requests/api/v1/registrations_requests_spec.rb
+++ spec/requests/api/v1/registrations_requests_spec.rb
@@ -5,10 +5,12 @@ require 'rails_helper'
 
 describe 'Api::V1::Registrations', type: :request do
   context 'When creating a new user' do
-    let(:user) { build :user } # to save the email parameter and compare it later
+    let(:user) { build :user }
+    let(:expected_response) { { 'email' => user.email } }
     let(:params) do
       { user: { email: user.email,
-                password: user.password } }
+                password: user.password,
+                fullname: user.fullname } }
     end
 
     subject { post '/api/v1/signup', params: params }
@@ -17,17 +19,17 @@ describe 'Api::V1::Registrations', type: :request do
       expect { subject }.to change { User.count }.by(1)
       expect(response).to have_http_status(200)
       expect(response.headers['Authorization']).to be_present
-
       json_response = JSON.parse(response.body)
-      expect(json_response['data']).to have_attribute(:email).with_value(user.email)
+      puts json_response
+      expect(json_response['data']['attributes']).to include(expected_response)
     end
   end
 
-  context 'When an email already exists' do 
+  context 'When an email already exists' do
     let!(:existing_user) { create :user }
     let(:params) do
-        { user: { email: existing_user.email,
-                  password: existing_user.password } }
+      { user: { email: existing_user.email,
+                password: existing_user.password } }
     end
 
     subject { post '/api/v1/signup', params: params }
@@ -36,6 +38,5 @@ describe 'Api::V1::Registrations', type: :request do
       expect { subject }.not_to(change { User.count })
       expect(response).to have_http_status(400)
     end
-    
   end
 end
