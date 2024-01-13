62fdd76c1cf77946ce78d53f249732110a05bd2f
AlexDikun <loban.sasha.97@mail.ru>
2024-01-13 17:56:39 +0300

    Allow users to Sign Up

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -2,4 +2,11 @@
 # frozen_string_literal: true
 
 class Api::V1::BaseController < ApplicationController
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[fullname])
+  end
 end
