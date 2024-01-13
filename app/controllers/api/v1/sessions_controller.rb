e3fc9ed3ee375dbdbd1bc46bfd2ccf8044e11a94
AlexDikun <loban.sasha.97@mail.ru>
2024-01-13 23:01:57 +0300

    Allow users to Log In & Log Out

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,4 +2,17 @@
 # frozen_string_literal: true
 
 class Api::V1::SessionsController < Devise::SessionsController
+  private
+
+  def respond_with(resource, _opts = {})
+    if resource.errors.empty?
+      render jsonapi: resource, status: 200
+    else
+      render jsonapi_errors: resource.errors, status: 400
+    end
+  end
+
+  def respond_to_on_destroy
+    head :no_content
+  end
 end
