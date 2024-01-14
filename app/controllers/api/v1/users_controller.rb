4bf869f29fc18cdcb1cf442cc620be62c8bd2368
AlexDikun <loban.sasha.97@mail.ru>
2024-01-14 18:52:10 +0300

    SHOW UPDATE DESTROY actions in UsersController are defined

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,12 +2,41 @@
 # frozen_string_literal: true
 
 class Api::V1::UsersController < Api::V1::BaseController
-  # GET api/v1/users
-  def show; end
+  before_action :set_user, only: %i[show update destroy]
 
-  # PATCH/PUT api/v1/users
-  def update; end
+  # GET api/v1/users/:id
+  def show
+    render jsonapi: @user
+  end
 
-  # DELETE api/v1/users
-  def destroy; end
+  # PATCH/PUT api/v1/users/:id
+  def update
+    if @user.update(user_update_params)
+      @user.avatar_derivatives! && @user.save if @user.avatar
+      render jsonapi: @user, status: :ok, code: '200'
+    else
+      render jsonapi_errors: @user.errors, 
+                 status: :unprocessable_entity, code: '422'
+    end
+  end
+
+  # DELETE api/v1/users/:id
+  def destroy
+    if @user.destroy
+      render jsonapi: @user, status: :ok, code: '200'
+    else
+      render jsonapi_errors: @user.errors, 
+             status: :unprocessable_entity, code: '422'
+    end
+  end
+
+  private
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_update_params
+    params.require(:user).permit(:fullname, :email, :avatar)
+  end
 end
