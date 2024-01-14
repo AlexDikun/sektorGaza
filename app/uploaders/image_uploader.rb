8319f4085afb8a53c3a15e75378afd52dcce9344
AlexDikun <loban.sasha.97@mail.ru>
2024-01-14 19:32:04 +0300

    added default_url plugin for Shrine uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,6 +7,7 @@ class ImageUploader < Shrine
   plugin :validation_helpers
   plugin :derivatives
   plugin :determine_mime_type
+  plugin :default_url
 
   Attacher.validate do
     validate_extension %w[jpg jpeg png webp]
@@ -18,5 +19,9 @@ class ImageUploader < Shrine
       product_image: magick.resize_to_limit!(400, 400),
       user_avatar: magick.resize_to_limit!(100, 100)
     }
+  end
+
+  Attacher.default_url do |**options|
+    "/placeholders/sektor_default.jpg"
   end
 end
