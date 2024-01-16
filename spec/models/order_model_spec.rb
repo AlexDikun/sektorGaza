90e76deaef4d4182fa84214590e1bce68a510cd5
AlexDikun <loban.sasha.97@mail.ru>
2024-01-16 11:31:08 +0300

    wrote test cases for validations of the Order model

--- spec/models/order_model_spec.rb
+++ spec/models/order_model_spec.rb
@@ -4,4 +4,11 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_length_of(:name).is_at_most(30) }
+  it { is_expected.to validate_presence_of(:address) }
+  it { is_expected.to validate_length_of(:address).is_at_most(200) }
+  it { is_expected.to validate_presence_of(:telephone_number) }
+  it { is_expected.to validate_length_of(:telephone_number).is_at_most(18) }
+  it { is_expected.to belong_to(:user) }
 end
