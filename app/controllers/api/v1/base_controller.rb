5e50dd74fa12795d9200c36c1f44594ae86d79da
AlexDikun <loban.sasha.97@mail.ru>
2024-01-16 09:39:22 +0300

    added authenticate_user helper

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -9,4 +9,14 @@ class Api::V1::BaseController < ApplicationController
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[fullname])
   end
+
+  private
+
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to new_user_session_path, format: :json
+    end
+  end
 end
