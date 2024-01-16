8be31c5b8b4e7477949b57a1cd24755d93a0e327
AlexDikun <loban.sasha.97@mail.ru>
2024-01-16 11:43:01 +0300

    added OrdersController

---
+++ app/controllers/api/v1/orders_controller.rb
@@ -0,0 +1,10 @@
+# app/controllers/api/v1/orders_controller.rb
+# frozen_string_literal: true
+
+class Api::V1::OrdersController < ApplicationController
+  # GET api/v1/users/:user_id/orders/:id
+  def show; end
+
+  # POST api/v1/users/:user_id/orders
+  def create; end
+end
---
+++ spec/requests/api/v1/orders_requests_spec.rb
@@ -0,0 +1,12 @@
+# spec/requests/api/orders_requests_spec.rb
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Api::V1::Orders', type: :request do
+  describe 'GET api/v1/users/:user_id/orders/:id' do
+  end
+
+  describe 'POST api/v1/users/:user_id/orders' do
+  end
+end
