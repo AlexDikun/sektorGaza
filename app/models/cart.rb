f70d602157b5cdd405ca09951e71593eb6a4944b
AlexDikun <loban.sasha.97@mail.ru>
2024-01-26 13:32:12 +0300

    wrote a validation tests for the Cart model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -13,4 +13,6 @@
 
 class Cart < ApplicationRecord
   belongs_to :user
+  has_many :line_items, dependent: :destroy
+  has_many :products, through: :line_items
 end
--- spec/models/cart_model_spec.rb
+++ spec/models/cart_model_spec.rb
@@ -5,4 +5,6 @@ require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
   it { is_expected.to belong_to :user }
+  it { is_expected.to have_many(:line_items).dependent(:destroy)  }
+  it { is_expected.to have_many(:products).through(:line_items) }
 end
