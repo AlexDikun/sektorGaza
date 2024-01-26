2967bf908be82f5006cb3f94f2e45aba1814047a
AlexDikun <loban.sasha.97@mail.ru>
2024-01-26 20:11:53 +0300

    defined 'carts#destroy' action'

--- app/controllers/api/v1/carts_controller.rb
+++ app/controllers/api/v1/carts_controller.rb
@@ -2,6 +2,16 @@
 # frozen_string_literal: true
 
 class Api::V1::CartsController < Api::V1::BaseController
+  before_action :authenticate_user!, only: :destroy
+  before_action :set_cart, only: :destroy
+
   # DELETE api/v1/carts/:id
-  def destroy; end
+  def destroy
+    if @current_cart.destroy
+      render jsonapi: @current_cart, status: :ok, code: '200'
+    else
+      render jsonapi_errors: @current_cart.errors,
+             status: :unprocessable_entity, code: '422'
+    end
+  end
 end
