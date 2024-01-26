0c9eb0c7faede1f66ad3d572fb020d8e63f775e3
AlexDikun <loban.sasha.97@mail.ru>
2024-01-26 17:49:10 +0300

    added rooutes and controller for 'carts#destroy'

---
+++ app/controllers/api/v1/carts_corntoller.rb
@@ -0,0 +1,7 @@
+# app/controllers/api/v1/carts_controller.rb
+# frozen_string_literal: true
+
+class Api::V1::CartsController < Api::V1::BaseController
+  # DELETE api/v1/carts/:id
+  def destroy; end
+end
