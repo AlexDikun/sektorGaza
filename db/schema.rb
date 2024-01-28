eed9862f6b4a168aca32ec98f88f09a901f4d56f
AlexDikun <loban.sasha.97@mail.ru>
2024-01-28 20:37:19 +0300

    added order reference to LineItems

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_240_128_172_226) do
+ActiveRecord::Schema.define(version: 20_240_128_173_326) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 20_240_128_172_226) do
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
     t.integer 'cart_id'
+    t.bigint 'order_id'
+    t.index ['order_id'], name: 'index_line_items_on_order_id'
     t.index %w[product_id cart_id], name: 'index_line_items_on_product_id_and_cart_id', unique: true
   end
 
@@ -82,5 +84,6 @@ ActiveRecord::Schema.define(version: 20_240_128_172_226) do
   add_foreign_key 'categorizations', 'categories'
   add_foreign_key 'categorizations', 'products'
   add_foreign_key 'line_items', 'carts'
+  add_foreign_key 'line_items', 'orders'
   add_foreign_key 'line_items', 'products'
 end
