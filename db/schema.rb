48527ab2bb6b80034a9cdcedbed2072c4bd13e31
AlexDikun <loban.sasha.97@mail.ru>
2024-01-28 20:27:04 +0300

    change in the design of the orders model: removed the foreign key in Orders that refers to the user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_240_126_085_244) do
+ActiveRecord::Schema.define(version: 20_240_128_172_226) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -55,7 +55,6 @@ ActiveRecord::Schema.define(version: 20_240_126_085_244) do
     t.string 'name'
     t.string 'telephone_number'
     t.text 'address'
-    t.integer 'user_id'
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
   end
@@ -84,5 +83,4 @@ ActiveRecord::Schema.define(version: 20_240_126_085_244) do
   add_foreign_key 'categorizations', 'products'
   add_foreign_key 'line_items', 'carts'
   add_foreign_key 'line_items', 'products'
-  add_foreign_key 'orders', 'users'
 end
