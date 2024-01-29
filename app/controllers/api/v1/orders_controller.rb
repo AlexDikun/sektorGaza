baeda45ae8a259a86c139d9b6321117805e68af2
AlexDikun <loban.sasha.97@mail.ru>
2024-01-29 21:50:02 +0300

    wrote simple test for 'order#show' and defined action it

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -4,9 +4,13 @@
 class Api::V1::OrdersController < Api::V1::BaseController
   before_action :authenticate_user!, only: %i[show create]
   before_action :set_cart, only: :create
+  before_action :set_order, only: :show
 
   # GET api/v1/orders/:id
-  def show; end
+  def show
+    render json: OrderSerializer.new(@order, include: [:line_items])
+                                .serializable_hash.to_json, status: :ok, code: '200'
+  end
 
   # POST api/v1/orders
   def create
@@ -29,5 +33,9 @@ class Api::V1::OrdersController < Api::V1::BaseController
 
   def order_params
     params.require(:order).permit(:name, :telephone_number, :address)
+  end
+
+  def set_order
+    @order = Order.find(params[:id])
   end
 end
--- spec/requests/api/v1/orders_requests_spec.rb
+++ spec/requests/api/v1/orders_requests_spec.rb
@@ -4,13 +4,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Orders', type: :request do
+  let(:user) { create :user }
+  before { sign_in user }
+
   context 'place an order' do
-    let(:user) { create :user }
     let(:cart) { create :cart, user_id: user.id }
     let(:product) { create :product }
     let!(:item) { create :line_item, cart_id: cart.id, product_id: product.id }
-
-    before { sign_in user }
 
     describe 'POST api/v1/orders' do
       let(:params) { { order: attributes_for(:order) } }
@@ -20,6 +20,23 @@ RSpec.describe 'Api::V1::Orders', type: :request do
         expect { subject }.to change { Order.count }.by(1)
         expect(response).to have_http_status(201)
       end
+    end
+  end
+
+  describe 'GET api/v1/orders/:id' do
+    let(:order) { create :order }
+    let(:params) { { id: order.id } }
+    let(:expected_response) { { 'telephone_number' => order.telephone_number } }
+
+    subject { get api_v1_order_path(params) }
+
+    it 'show order' do
+      subject
+      expect(response).to have_http_status(200)
+
+      json_response = JSON.parse(response.body)
+      puts json_response
+      expect(json_response['data']['attributes']).to include(expected_response)
     end
   end
 end
