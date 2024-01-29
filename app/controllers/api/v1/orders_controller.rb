2cbae4b05ce86bb155ff84af275b5a8bbf4c9ba1
AlexDikun <loban.sasha.97@mail.ru>
2024-01-29 15:57:10 +0300

    defined 'orders#create' action

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -3,10 +3,31 @@
 
 class Api::V1::OrdersController < Api::V1::BaseController
   before_action :authenticate_user!, only: %i[show create]
+  before_action :set_cart, only: :create
 
   # GET api/v1/orders/:id
   def show; end
 
   # POST api/v1/orders
-  def create; end
+  def create
+    @order = Order.new(order_params)
+
+    @current_cart.line_items.each do |item|
+      @order.line_items << item
+      item.cart_id = nil
+    end
+
+    if @order.save
+      render jsonapi: @order, status: :created, code: '201'
+    else
+      render jsonapi_errors: @order.errors,
+             status: :unprocessable_entity, code: '422'
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:name, :telephone_number, :address)
+  end
 end
