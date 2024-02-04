7f48d473132801d53eb8be0855ffb241e1789599
AlexDikun <loban.sasha.97@mail.ru>
2024-02-04 21:45:46 +0300

    added ReviewsController and routes for it

---
+++ app/controllers/api/v1/reviews_controller.rb
@@ -0,0 +1,19 @@
+# app/controllers/api/v1/reviews_controller.rb
+# frozen_string_literal: true
+
+class Api::V1::ReviewsController < Api::V1::BaseController
+  # GET api/v1/products/:product_id/reviews
+  def index; end
+
+  # GET api/v1/products/:product_id/reviews/:id
+  def show; end
+
+  # POST api/v1/products/:product_id/reviews
+  def create; end
+
+  # PATCH/PUT api/v1/products/:product_id/reviews/:id
+  def update; end
+
+  # DELETE api/v1/products/:product_id/reviews/:id
+  def destroy; end
+end
