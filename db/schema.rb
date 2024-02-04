17465050cea725fec656098d2619d608b6bb88a3
AlexDikun <loban.sasha.97@mail.ru>
2024-02-04 19:06:44 +0300

    added unique andex for scope user and product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_240_204_143_856) do
+ActiveRecord::Schema.define(version: 20_240_204_152_344) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 20_240_204_143_856) do
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
     t.index ['product_id'], name: 'index_reviews_on_product_id'
+    t.index %w[user_id product_id], name: 'index_reviews_on_user_id_and_product_id', unique: true
     t.index ['user_id'], name: 'index_reviews_on_user_id'
   end
 
